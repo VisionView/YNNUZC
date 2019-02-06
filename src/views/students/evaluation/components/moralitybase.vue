c812c56f089f33d294bc68f7ddb0c33bbd1759e6
Blank <zilisheng1996@gmail.com>
2019-02-06 14:49:47 +0800

    rerouter evaluation components

---
+++ src/views/students/evaluation/components/moralitybase.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-form ref="" :model="stuScoreForm">
+      <table class="form-table">
+        <tr>
+          <td rowspan="7" class="form-title">基础性素质</td>
+          <td colspan="12" class="small-title">(一)品德基础素质</td>
+        </tr>
+        <tr>
+          <td>政治表现</td>
+          <td>遵纪守法</td>
+          <td>社会公德</td>
+          <td>个人品质</td>
+          <td>人际关系</td>
+          <td>劳动表现</td>
+          <td>扣分</td>
+          <td colspan="5">扣分原因</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+          <td colspan="5">8</td>
+        </tr>
+      </table>
+      </el-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'StudentMoralityBase'
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
--- src/views/students/evaluation/sself.vue
+++ src/views/students/evaluation/sself.vue
@@ -1,25 +1,18 @@
 <template>
-  <div style="height: 300px;">
+  <div>
+    <div class="div-left">
     <el-steps direction="vertical" :active="1">
-      <el-step title="品德基础素质">
-        
-      </el-step>
-      <el-step title="身心基础素质">
-
-      </el-step>
-      <el-step title="品德发展素质">
-
-      </el-step>
-      <el-step title="社会实践素质">
-
-      </el-step>
-      <el-step title="科技创新素质">
-
-      </el-step>
-      <el-step title="职业综合素质">
-
-      </el-step>
+      <el-step title="品德基础素质"></el-step>
+      <el-step title="身心基础素质"></el-step>
+      <el-step title="品德发展素质"></el-step>
+      <el-step title="社会实践素质"></el-step>
+      <el-step title="科技创新素质"></el-step>
+      <el-step title="职业综合素质"></el-step>
     </el-steps>
+  </div>
+  <div class="div-right">
+    <router-view/>
+  </div>
   </div>
 </template>
 <script>
@@ -38,5 +31,11 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-
+.div-left
+  margin-top 50px
+  height 400px
+  float left
+.div-right
+  float left
+  margin-left 100px
 </style>
