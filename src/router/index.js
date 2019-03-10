d7af3558de085bef39f06cae08e2801e487ab8d3
Blank <zilisheng1996@gmail.com>
2019-03-10 19:34:35 +0800

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/',
+      name: 'home',
+      redirect: '/admin'
+    },
+    {
       path: '/login',
       name: 'login',
       meta: {
